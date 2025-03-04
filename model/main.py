2c93d6154c28a1309f71cf06fe314b5809edfefa
wjake <50133276+wjake@users.noreply.github.com>
2025-03-04 17:54:59 +0000

    Added UDP IPC & Gesture Stability Check

--- model/main.py
+++ model/main.py
@@ -2,6 +2,8 @@ import os
 import cv2 as cv
 import copy
 import argparse
+import socket
+import time
 
 from keypoint_classifier.keypoint_classifier import KeyPointClassifier
 
@@ -19,6 +21,15 @@ def get_args():
     parser.add_argument('--use_static_image_mode', action='store_true')
     parser.add_argument("--min_detection_confidence", type=float, default=0.7)
     parser.add_argument("--min_tracking_confidence", type=float, default=0.5)
+
+    # --- Written by Jake Wright (github.com/wjake), 2025 ---
+    # Add arguments for UDP socket
+    parser.add_argument("--server_address", type=str, default='127.0.0.1', help="UDP server address")
+    parser.add_argument("--server_port", type=int, default=65432, help="UDP server port")
+    parser.add_argument("--stability_threshold", type=int, default=2000,
+                        help="Threshold for prediction stability in milliseconds")
+    # --- End of code written by Jake Wright ----------------
+
     return parser.parse_args()
 
 def select_mode(key, mode):
@@ -67,6 +78,18 @@ def main():
     use_brect = True
     mode = 0
 
+    # --- Written by Jake Wright (github.com/wjake), 2025 ---
+    # Initialize the UDP socket & variables for prediction stability
+    server_address = (args.server_address, args.server_port)
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    last_pred = -1
+    curr_pred = -1
+    inter_pred = -1
+    pred_time = None
+    stability_threshold = args.stability_threshold / 1000.0
+    # --- End of code written by Jake Wright ----------------
+
     while True:
         fps = fps_calc.get()
 
@@ -97,18 +120,58 @@ def main():
                     # print(f"Mode: {mode} Data: {pre_processed_landmark_list}")
                     logging_csv(number, mode, pre_processed_landmark_list)
                     hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
-                    print(f"Sign text: {keypoint_classifier_labels[hand_sign_id]}, Hand Sign ID: {hand_sign_id}")
+                    # print(f"Sign text: {keypoint_classifier_labels[hand_sign_id]}, Hand Sign ID: {hand_sign_id}")
 
                 else:
                     hand_sign_id = -1
-
+                
                 label_text = keypoint_classifier_labels[hand_sign_id] if hand_sign_id >= 0 else "Unknown"
+
+                # --- Written by Jake Wright (github.com/wjake), 2025 ---
+                # Check if prediction is stable & send to socket
+                if curr_pred == hand_sign_id: 
+                    # If prediction is the same, calculate stability
+                    if pred_time is None:
+                        pred_time = time.time()
+                    elif time.time() - pred_time >= stability_threshold:
+                        # Check prediction is different from last prediction, or intermediate prediction exists
+                        if curr_pred != last_pred or inter_pred is not None:
+                            last_pred = curr_pred
+                            inter_pred = None
+
+                            # Send prediction to socket
+                            try:
+                                message = keypoint_classifier_labels[curr_pred].encode('utf-8')
+                                print(f"Sent hand_sign_label: {message} to {server_address}")
+                                udp_socket.sendto(message, server_address)
+                            except socket.timeout:
+                                print('Socket timeout')
+                else:
+                    # Reset timer if prediction changes, and track intermediate prediction
+                    print(f"Detected hand_sign_id: {hand_sign_id}, Label: {label_text}")
+                    pred_time = None
+                    if curr_pred != last_pred:
+                        inter_pred = curr_pred
+                    curr_pred = hand_sign_id
+                # --- End of code written by Jake Wright ----------------
+
                 debug_image = draw_bounding_rect(use_brect, debug_image, brect)
                 debug_image = draw_landmarks(debug_image, landmark_list)
                 debug_image = draw_info_text(debug_image, brect, handedness, label_text, "")
 
+        # --- Written by Jake Wright (github.com/wjake), 2025 ---
+        # Reset current prediction if no hands are detected
+        else:
+            curr_pred = -1
+        # --- End of code written by Jake Wright ----------------
+
         debug_image = draw_info(debug_image, fps, mode, number)
         cv.imshow('Hand Gesture Recognition', debug_image)
+
+    # --- Written by Jake Wright (github.com/wjake), 2025 ---
+    # Close the socket
+    udp_socket.close()
+    # --- End of code written by Jake Wright ----------------
 
     camera.release()
     cv.destroyAllWindows()
